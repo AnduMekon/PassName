ee5f6d8c7b49de4fd36809b87536783cb1e661b6
AnduMekon <anduabe@yahoo.com>
2020-04-24 08:28:09 -0400

    formSelect

--- src/playground/formSelect.js
+++ src/playground/formSelect.js
@@ -1,4 +1,63 @@
+//https://www.youtube.com/watch?v=7Vo_VCcWupQ
+
 import React from 'react';
+import ReactDOM from 'react-dom';
+
+class Form extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            userName:''
+          }
+    }
+    userNameHandler (event) {
+        this.state({
+          userName: event.target.value
+        })
+        console.warn(event)
+    }
+
+    render() { 
+        return ( 
+            <form>
+                <div>
+                <label>userName</label>
+                <input type='text' value={this.state.userName} onChange={this.userNameHandler} />
+                </div>
+                <div>
+                </div>
+            
+            
+            
+            
+            </form>
+
+        );
+    }
+}
+ 
+ReactDOM.render(<Form />, document.getElementById('app'))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* import React from 'react';
 import ReactDOM from 'react-dom';
 
 
@@ -6,26 +65,68 @@ class Form extends React.Component {
     constructor(props) {
         super(props);
         this.state = { 
-            userName: ''
+            userName: '',
+            comments:'',
+            topics:''
          }
     }
 
     userNameHandler = (event) => {
         this.setState({
-            userName: event.target.value
+            userName: event.target.value,
+            
         })
        
     }
-     
-  
+    commentsHandler= event => {
+        this.setState({
+            comments : event.target.value,
+            
+        })
+       
+    }
+  topichandler = event => {
+        this.setState({
+        topics: event.target.value,
+            
+        })
+       
+    }
+    formHandler = event => {
+        
+    alert(`${this.state.userName} ${this.state.comments} ${this.state.topics}`)
+        event.preventDefault()
+    }
     render() { 
         return ( 
-            <form>
+            <form onSubmit={this.formHandler}>
                 <div>
                     <label>User Name</label>
-                    <input type="text" value={this.state.userName} onChange={this.userNameHandler}/>
+                    <input 
+                        type="text" 
+                        value={this.state.userName} 
+                        onChange={this.userNameHandler}/>
                 
                 </div>
+
+                <div>
+                    <label>Comment</label>
+                    <textarea 
+                        value={this.state.comments} 
+                        onChange={this.commentsHandler}></textarea>
+                </div>
+                <div>
+                    <label>Topics</label>
+                    <select value={this.state.topics} 
+                            onChange={this.topichandler}>
+                        <option>one</option>
+                        <option>two</option>
+                        <option>three</option>
+                    </select>
+                </div>
+                <div>
+                    <button>Submit</button>
+                 </div>
             
             
             </form>
@@ -33,4 +134,5 @@ class Form extends React.Component {
     }
 }
  
-ReactDOM.render(<Form />, document.getElementById('app'))
+ReactDOM.render(<Form />, document.getElementById('app'))*/
+
